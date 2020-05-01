c0ada4ae545e86ed5cd984191a650e46b813d604
fernandorivdev <fernandoevelio@hotmail.es>
2020-05-01 10:58:06 -0600

    Updates

---
+++ precache-manifest.819252464cd0bed7aa588286ffbe19de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63bf51b75077ed6cd79825df5c877a1a",
+    "url": "/fernandorivera_webcodechallenge/index.html"
+  },
+  {
+    "revision": "05307a135833f32d9d8f",
+    "url": "/fernandorivera_webcodechallenge/static/css/main.adcd094e.chunk.css"
+  },
+  {
+    "revision": "3c560c5f42be7d6bf379",
+    "url": "/fernandorivera_webcodechallenge/static/js/2.bcd6397a.chunk.js"
+  },
+  {
+    "revision": "5a8df5d16df44d8989484796087caa60",
+    "url": "/fernandorivera_webcodechallenge/static/js/2.bcd6397a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05307a135833f32d9d8f",
+    "url": "/fernandorivera_webcodechallenge/static/js/main.ace0af57.chunk.js"
+  },
+  {
+    "revision": "edb0d3d3a83d21332bc6",
+    "url": "/fernandorivera_webcodechallenge/static/js/runtime-main.8a823f60.js"
+  }
+]);
